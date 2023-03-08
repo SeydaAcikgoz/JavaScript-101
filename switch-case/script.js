36b543d873cf846d7b10e7ed763939d4437f1fc0
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-03-08 21:09:29 +0300

    Add files via upload

---
+++ switch-case/script.js
@@ -0,0 +1,23 @@
+var sayi1 = document.getElementById("sayi1");
+var sayi2 = document.getElementById("sayi2");
+
+function hesapla(type) {
+    var sonuc = "";
+
+    if (type === "+") {
+        sonuc = Number(sayi1.value) + Number(sayi2.value);
+    }
+    else if (type === "-") {
+        sonuc = Number(sayi1.value) - Number(sayi2.value);
+    }
+
+    else if (type === "*") {
+        sonuc = Number(sayi1.value) * Number(sayi2.value);
+    }
+
+    else if (type === "/") {
+        sonuc = Number(sayi1.value) / Number(sayi2.value);
+    }
+    document.getElementById("sonuc").innerHTML = sonuc;
+
+}
