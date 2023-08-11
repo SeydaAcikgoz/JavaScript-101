90bc1547685c393fe0f1dce8d28520afdcb30f0d
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-08-11 17:58:22 +0300

    Add files via upload

---
+++ hazır_fonksiyonlar/script.js
@@ -0,0 +1,15 @@
+var isim="Seyda";
+document.writeln(typeof isim);
+document.writeln(typeof(isim));
+
+
+var deger1=10;
+var deger2=40;
+var sonuc1 = "deger1+deger2";
+var sonuc2 = eval("deger1+deger2");
+document.write(sonuc1);
+document.write(sonuc2);
+function deneme(){
+    document.write("Merhaba");
+}
+var komut = eval("deneme()");
