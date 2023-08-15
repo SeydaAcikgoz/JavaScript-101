02d33a37de5ffcd2d3efa8e3dc835040c4d96533
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-08-15 21:40:14 +0300

    aded includes()

--- hazır_fonksiyonlar2/script.js
+++ hazır_fonksiyonlar2/script.js
@@ -5,3 +5,8 @@ document.write(sonuc);
 sonuc = icerik.search(/Script/i);   //içeriğin büyük-küçük harf olmasına bakmaz
 document.write(sonuc + "<br>");
 
+//includes():kendisine parametre olarak verilen değer doğrultusunda değişken içeriğini kontrol ederek boolean veri türünde sonuç döndürür.
+var metin ="Javascript eğitim seti";
+var metin2=metin.includes("seti");
+document.write(metin2 + "<br>");
+
