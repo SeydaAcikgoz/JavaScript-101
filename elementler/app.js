6969248340b87886ab102e3dba1a7ef1b1aeaa07
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-06 13:18:39 +0300

    Add files via upload

---
+++ elementler/app.js
@@ -0,0 +1,36 @@
+//Dinamik Olarak Element Oluşturmak
+const cardBody = document.querySelectorAll(".card-body")[1];
+const link = document.createElement("a");
+link.id="goBlogWebSite";
+link.className="btn btn-dark btn-sm mt-3";
+link.href="http://enesbayram.net";
+link.target="_blank";
+link.innerHTML = "Kişisel Websiteme Git";
+
+cardBody.appendChild(link);
+
+/*ORNEK
+<li class="list-group-item d-flex justify-content-between">Todo 3
+<a href="#" class="delete-item">
+    <i class="fa fa-remove"></i>
+</a>
+</li>
+*/
+const todoList = document.querySelector(".list-group");
+const todo = document.createElement("li");
+const todoLink = document.createElement("a");
+const i  = document.createElement("i");
+
+todo.className="list-group-item d-flex justify-content-between";
+todo.innerHTML = "Todo 5";
+
+todoLink.href="#";
+todoLink.className="delete-item";
+
+i.className="fa fa-remove";
+
+todoLink.appendChild(i);
+todo.appendChild(todoLink);
+todoList.appendChild(todo);
+
+console.log(link);
