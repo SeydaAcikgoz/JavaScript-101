d5d49392a20c8f5b3ab1270b1e9acdab69b6fee7
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-20 19:12:06 +0300

    Add files via upload

---
+++ for_in_for_of_loops/app.js
@@ -0,0 +1,20 @@
+//For in  - For of Döngüleri
+
+let names = ["Enes","Ali","Yusuf","Betül","Ceyda"];
+
+// names.forEach(function(name){
+//     console.log(name)
+// })
+// names.forEach(name=>console.log(name))//arrow function
+
+
+//For in Döngüsü->index numarasını verir
+// for(let name in names){
+//     console.log(name , names[name])
+// }
+
+
+//For of Döngüsü->index değerini verir
+// for(let isim of names){
+//     console.log(isim , names.indexOf(isim))
+// }
