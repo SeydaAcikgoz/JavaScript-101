c16cb2de747afc358d6c8f641f7ab89e3220b2ff
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-21 14:14:43 +0300

    Add files via upload

---
+++ dizi_metotları/script.js
@@ -0,0 +1,9 @@
+//length metodu
+var renkler=["siyah","sarı","beyaz","kırmızı","mavi"];
+document.writeln("Dizinin eleman sayisi:"+renkler.length+"<br>");
+
+var renkler2 = ["siyah", "sarı",["lila","lacivert"], "beyaz", "kırmızı", "mavi"];
+document.writeln("Dizinin eleman sayisi:" + renkler2.length + "<br>");
+document.writeln("Alt dizinin eleman sayisi:" + renkler2[2].length + "<br>");
+
+
