1cf3c0c5cdb3b63564290692a53526bd0bb549bd
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-21 14:18:02 +0300

    added delete

--- dizi_metotları/script.js
+++ dizi_metotları/script.js
@@ -1,9 +1,10 @@
 //length metodu
 var renkler=["siyah","sarı","beyaz","kırmızı","mavi"];
 document.writeln("Dizinin eleman sayisi:"+renkler.length+"<br>");
-
 var renkler2 = ["siyah", "sarı",["lila","lacivert"], "beyaz", "kırmızı", "mavi"];
 document.writeln("Dizinin eleman sayisi:" + renkler2.length + "<br>");
 document.writeln("Alt dizinin eleman sayisi:" + renkler2[2].length + "<br>");
 
-
+//delete metodu:index silinmez değeri silinir.
+delete renkler[0];
+document.writeln(renkler +"<br>");
