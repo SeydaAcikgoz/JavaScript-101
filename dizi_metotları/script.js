4aacbc26139ac746a6183eeb08f6c6a2735451a0
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-21 14:27:01 +0300

    added pop()

--- dizi_metotları/script.js
+++ dizi_metotları/script.js
@@ -8,3 +8,7 @@ document.writeln("Alt dizinin eleman sayisi:" + renkler2[2].length + "<br>");
 //delete metodu:index silinmez değeri silinir.
 delete renkler[0];
 document.writeln(renkler +"<br>");
+
+//pop():dizi içindeki son elemanı siler ve sildiği elemanın değerini geri döndürür.
+renkler.pop();
+document.writeln(renkler + "<br>");
